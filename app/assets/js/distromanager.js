5d376cb147f3d9dd7e0a45b8bcb6ce5e12b5e077
DucFilm <leobrouart72@gmail.com>
2023-06-25 14:52:04 +0200

    final launcher

--- app/assets/js/distromanager.js
+++ app/assets/js/distromanager.js
@@ -3,8 +3,8 @@ const { DistributionAPI } = require('helios-core/common')
 const ConfigManager = require('./configmanager')
 
 // Old WesterosCraft url.
-// exports.REMOTE_DISTRO_URL = 'http://mc.westeroscraft.com/WesterosCraftLauncher/distribution.json'
-exports.REMOTE_DISTRO_URL = 'https://helios-files.geekcorner.eu.org/distribution.json'
+// exports.REMOTE_DISTRO_URL = 'https://raw.githubusercontent.com/DucFilm/COZ/c669351863a90adbeecde7dd0f7e22b5a9596e84/distribution.json'
+exports.REMOTE_DISTRO_URL = 'https://raw.githubusercontent.com/DucFilm/COZ/c669351863a90adbeecde7dd0f7e22b5a9596e84/distribution.json'
 
 const api = new DistributionAPI(
     ConfigManager.getLauncherDirectory(),
